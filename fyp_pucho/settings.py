f4e0485101faa999c99f2d5aba4f0893ce0d7a3d
Saad Hassan <saad.hassan@convertdigital.com.au>
2022-06-22 20:20:00 +0500

    updated

--- fyp_pucho/settings.py
+++ fyp_pucho/settings.py
@@ -113,13 +113,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# Setting media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
