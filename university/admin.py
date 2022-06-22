896e023d48c582b8e27e10e62e22fe78eccd0608
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-06-22 21:41:12 +0500

    Update admin.py

--- university/admin.py
+++ university/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import University
 # Register your models here.
 
-  admin.site.register(University)
+admin.site.register(University)
 
 # Register your models here.
