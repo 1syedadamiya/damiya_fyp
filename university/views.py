6125bbbfb8f83c257ed898b243d49d6279563d14
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-07-14 18:02:43 +0500

    Update views.py

--- university/views.py
+++ university/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def gcuni(request):
+    return render(request, "GCuni.html")
+  
+def ituuni(request):
+    return render(request, "ITUuni.html")
+  
+def puuni(request):
+    return render(request, "PUuni.html")
+
+def kcuni(request):
+    return render(request, "KCuni.html")
+  
+def lcwuuni(request):
+    return render(request, "LCWUuni.html")
+  
+def uetuni(request):
+    return render(request, "UETuni.html")
+  
+def ueuni(request):
+    return render(request, "UEuni.html")
