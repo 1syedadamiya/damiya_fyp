8c502c717084c93534a5d8ba586a5a3c79133b33
anoosha1228 <atanseer2016@gmail.com>
2022-07-15 23:22:04 +0500

    static

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import form, profile
+from .views import form, profile, home
 from . import views
 urlpatterns = [
     path('form/', form, name="form"),
     path('profile/', profile, name="profile"),
+    path('home/', home, name="home"),
 ]
    
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+
 def form(request):
     return render(request, "form.html")
 
+
 def profile(request):
     return render(request, "profile.html")
+
+
+def home(request):
+    return render(request, "home.html")
