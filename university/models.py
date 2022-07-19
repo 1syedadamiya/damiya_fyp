5840904b4cd249a6aa38661d6d93b9295f69b842
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-07-19 21:22:10 +0500

    Update models.py

--- university/models.py
+++ university/models.py
@@ -5,7 +5,7 @@ class University (models.Model):
     university_name = models.CharField(max_length=50)
     program_name = models.CharField(max_length=50)
     last_merit = models.IntegerField()
-    eligibilty_criteria = models.CharField(max_length=100)
+    eligibilty_criteria = models.CharField(max_length=500)
     fee_structure = models.IntegerField()
     
 
