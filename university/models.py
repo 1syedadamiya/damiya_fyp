2d9e43cda7612b1efff5614cc31149acfd7d7460
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-07-21 01:29:05 +0500

    Update models.py

--- university/models.py
+++ university/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class University (models.Model):
     university_name = models.CharField(max_length=50)
     program_name = models.CharField(max_length=50)
-    last_merit = models.IntegerField()
+    last_merit = models.CharField(max_length=50)
     eligibilty_criteria = models.CharField(max_length=1500)
     fee_structure = models.IntegerField()
     
