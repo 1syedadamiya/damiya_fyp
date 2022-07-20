a41ba3919c9385bd386b846365c3b1b55f8bbc05
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-07-20 06:37:40 +0500

    Update models.py

--- university/models.py
+++ university/models.py
@@ -5,9 +5,9 @@ class University (models.Model):
     university_name = models.CharField(max_length=50)
     program_name = models.CharField(max_length=50)
     last_merit = models.IntegerField()
-    eligibilty_criteria = models.CharField(max_length=500)
+    eligibilty_criteria = models.CharField(max_length=1500)
     fee_structure = models.IntegerField()
     
 def __str__(self):
-        return self.university_name
+        return self.name
 # Create your models here.
