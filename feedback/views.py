e5cea07ff5ebc64323a624f80424946467326c48
1syedadamiya <41992188+1syedadamiya@users.noreply.github.com>
2022-07-23 00:44:53 +0500

    Update views.py

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
+@login_required(login_url='login')
 def feedback(request):
     return render(request, "feedback.html")
 # Create your views here.
